d75270125660bf84487bfae402e548f9ffaa8c1d
tmariyah <tanveermariyah@gmail.com>
2024-06-21 23:37:43 +0500

    added book exercises to the folder: exercise

---
+++ lib/exercises/color.ex
@@ -0,0 +1,4 @@
+defmodule Color do
+  def hex_code(:blue), do: "#0000FF"
+  def hex_code(:black), do: "#FFFFFF"
+end
---
+++ lib/exercises/spinner.ex
@@ -0,0 +1,61 @@
+defmodule Spinner do
+  def swap({row, col}) do
+    {col, row}
+  end
+
+  def flip_left_right({row, col}) do
+    {row, 5 - col}
+  end
+
+  def flip_top_bottom({row, col}) do
+    {5 - row, col}
+  end
+
+  def rotate(point, 90) do
+    point
+    |> swap()
+    |> flip_left_right()
+  end
+
+  def rotate(point, 180) do
+    point
+    |> flip_top_bottom()
+    |> flip_left_right()
+  end
+
+  def rotate(point, 270) do
+    point
+    |> rotate(180)
+    |> rotate(90)
+  end
+end
+
+points =
+  [
+    Point.new(2, 2),
+    Point.new(3, 2),
+    Point.new(4, 2),
+    Point.new(4, 3)
+  ]
+
+Canvas.tetromino(points)
+
+points
+|> Enum.map(&Spinner.flip_left_right/1)
+|> Canvas.tetromino()
+
+points
+|> Enum.map(&Spinner.flip_top_bottom()/1)
+|> Canvas.tetromino()
+
+points
+|> Enum.map(&Spinner.rotate(&1, 180))
+|> Canvas.tetromino()
+
+points
+|> Enum.map(&Spinner.rotate(&1, 90))
+|> Canvas.tetromino()
+
+points
+|> Enum.map(&Spinner.rotate(&1, 270))
+|> Canvas.tetromino()
