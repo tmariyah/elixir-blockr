9d0b4583b2a1835ff96b0abcb98692ed8db6e3ed
tmariyah <tanveermariyah@gmail.com>
2024-06-27 12:37:07 +0500

    add group functions

--- lib/group.ex
+++ lib/group.ex
@@ -1,55 +1,31 @@
 defmodule Group do
   def move_down(points) do
-    points
-    |> Enum.map(&Point.move_down/1)
+    Enum.map(points, &Point.move_down/1)
   end
 
   # move in the correct direction by changing the column in point
   def move_left(points) do
-    points
-    |> Enum.map(&Point.move_left/1)
+    Enum.map(points, &Point.move_left/1)
   end
 
   # move in the correct direction by changing the column in point
   def move_right(points) do
-    points
-    |> Enum.map(&Point.move_right/1)
+    Enum.map(points, &Point.move_right/1)
   end
 
   def swap(points) do
-    points
-    |> Enum.map(&Point.swap/1)
+    Enum.map(points, &Point.swap/1)
   end
 
   def flip_left_right(points) do
-    points
-    |> Enum.map(&Point.flip_left_right/1)
+    Enum.map(points, &Point.flip_left_right/1)
   end
 
   def flip_top_bottom(points) do
-    points
-    |> Enum.map(&Point.flip_top_bottom/1)
+    Enum.map(points, &Point.flip_top_bottom/1)
   end
 
-  def rotate(points, 0) do
-    points
-  end
-
-  def rotate(points, 90) do
-    points
-    |> Enum.map(&Point.swap/1)
-    |> Enum.map(&Point.flip_left_right/1)
-  end
-
-  def rotate(points, 180) do
-    points
-    |> Enum.map(&Point.flip_top_bottom/1)
-    |> Enum.map(&Point.flip_left_right/1)
-  end
-
-  def rotate(points, 270) do
-    points
-    |> Enum.map(&Point.swap/1)
-    |> Enum.map(&Point.flip_top_bottom/1)
+  def rotate(points, degree) do
+    Enum.map(points, &Point.rotate(&1, degree))
   end
 end
